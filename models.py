3349b58543c23e6b912d425eb69a903705bb9a01
FaerVeterok <109340831+FaerVeterok@users.noreply.github.com>
2022-08-15 12:40:10 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -4,11 +4,11 @@ from django.urls import reverse
 
 
 class Novosti(models.Model):
-     Persona = models.CharField(max_length=30,
+     Persona = models.CharField(max_length=60,
                             help_text="Введите атвора публикаций ",
                             verbose_name="Автор Публикаций")
 
-     Data = models.CharField(max_length=20,
+     Data = models.DateField(max_length=30,
                         help_text="Введите дату публицакий ",
                         verbose_name="Дата публикация")
 
@@ -16,23 +16,36 @@ class Novosti(models.Model):
          return self.name
 
 class Opisanie(models.Model):
-     title = models.CharField(max_length=200,
+     title = models.CharField(max_length=10000,
                              help_text="Описание Новостей",
                              verbose_name="Описание")
-     summary = models.TextField(max_length=1000,
-                               help_text="Введите заголовок Новостей", verbose_name="Заголовок Новостей ")
+     summary = models.TextField(max_length=75,
+                             help_text="Введите заголовок Новостей", verbose_name="Заголовок Новостей ")
+
+class Dobavlenie(models.Model):
+     Sekretari = models.ForeignKey('opisanie', on_delete=models.CASCADE,
+                             help_text = "Секретарь может добавить свой собственное описание",
+                             verbose_name = "Добавление описание Секретаря", null = True)
+
+     name = models.CharField(max_length=100,
+                             help_text = "Введите имя Секретаря",
+                             verbose_name = "Имя Секретаря")
+     release_date = models.DateField(max_length=30,
+                             help_text="Введите дату публицакий ",
+                             verbose_name="Дата публикация")
 
 
     def __str__(self):
         return self.name
 
 class Document(models.Model):
-    Data = models.CharField(max_length=20,
-                            help_text="Введите дату публицакий ",
-                            verbose_name="Дата публикация")
-    Persona = models.CharField(max_length=30,
-                               help_text="Введите атвора публикаций ",
-                               verbose_name="Автор Публикаций")
+    Data = models.DateField(max_length=30,
+                        help_text="Введите дату публицакий ",
+                        verbose_name="Дата публикация")
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50,
+                            help_text="Введите атвора публикаций ",
+                            verbose_name="Автор Публикаций")
 
 
     def __str__(self):
